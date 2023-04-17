9622f2dce21b94c9dd2d5a4cb0110f8c79814242
Nam <luyennam2001@gmail.com>
2023-04-17 17:26:46 +0700

    initial app

---
+++ CRUD-ConsoleApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace CRUD_ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            SqlConnection sqlConnection;
+            String connectionString = @"Data Source=NARPAVI;Initial Catalog=CoDb;Integrated Security=True";
+            try
+            {
+                sqlConnection = new SqlConnection(connectionString);
+                sqlConnection.Open();
+                Console.WriteLine("Database connected succesfully!");
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine(error.Message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
